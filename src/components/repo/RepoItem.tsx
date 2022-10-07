8fd11e273419405c026477448cabe45ebec871b7
kimjungjin <jungks9351@gmail.com>
2022-10-07 20:12:42 +0900

    remove : 사용하지 앟는 코드 제거

--- src/components/repo/RepoItem.tsx
+++ src/components/repo/RepoItem.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 import RepoName from 'component/repo/RepoName'
 import RepoDescription from 'component/repo/RepoDescription'
