a358885cd19f2d64f2b9fa334ed84223d55946a2
kimjungjin <jungks9351@gmail.com>
2022-10-07 22:39:45 +0900

    style : RepoName 스타일 수정

--- src/components/repo/RepoName.tsx
+++ src/components/repo/RepoName.tsx
@@ -3,13 +3,19 @@ import styled from 'styled-components'
 
 type ReponameProps = {
   children: string
+  url: string
 }
 
-const RepoName = ({ children }: ReponameProps) => {
-  return <RepoNameWrapper>{children}</RepoNameWrapper>
+const RepoName = ({ children, url }: ReponameProps) => {
+  return (
+    <a href={url} target="_blank" rel="noreferrer">
+      <RepoNameWrapper>{children}</RepoNameWrapper>
+    </a>
+  )
 }
 
 const RepoNameWrapper = styled.h3`
+  cursor: pointer;
   color: #0969da;
   font-size: 20px;
   &:hover {
