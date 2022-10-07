c86a1e88327c01cbae9995f602af194e6b75ef7b
kimjungjin <jungks9351@gmail.com>
2022-10-08 01:23:10 +0900

    feat : RepoDetail 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import RepoPage from 'pages/RepoPage'
 import MainPage from 'pages/MainPage'
+import RepoDetailPage from 'pages/RepoDetailPage'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/repo" element={<RepoPage />} />
+        <Route path="/repo">
+          <Route index element={<RepoPage />} />
+          <Route path=":owner/:name" element={<RepoDetailPage />} />
+        </Route>
         <Route path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
