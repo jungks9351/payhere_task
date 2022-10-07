3d045b7fdee137ba7b97a2a693ffdc6bbc7561ce
kimjungjin <jungks9351@gmail.com>
2022-10-08 01:34:38 +0900

    remove : 사용하지 않는 코드 제거

--- src/pages/RepoDetailPage.tsx
+++ src/pages/RepoDetailPage.tsx
@@ -1,15 +1,5 @@
 import IssueList from 'component/repo/IssueList'
 
-export type IssueType = {
-  title: string
-  html_url: string
-  repository_url: string
-  user: {
-    login: string
-    avatart_url: string
-  }
-}
-
 const RepoDetailPage = () => {
   return (
     <>
