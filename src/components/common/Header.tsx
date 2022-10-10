d0345fb4385b199868feed2f3353fbea3c749d2b
kimjungjin <jungks9351@gmail.com>
2022-10-11 04:43:56 +0900

    feat : 검색 결과 페이젱서 재검색 기능 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,18 +1,36 @@
-import { Link } from 'react-router-dom'
+import SearchForm from 'component/search/SearchRepoForm'
+import { Link, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Header = () => {
+  const { pathname } = useLocation()
+
   return (
     <HeaderWrapper>
       <Link to="/">
-        <h1>PAYHERE TASK</h1>
+        <Logo>PAYHERE TASK</Logo>
       </Link>
+      {pathname === '/repo' && (
+        <SearchFormWrapper>
+          <SearchForm />
+        </SearchFormWrapper>
+      )}
     </HeaderWrapper>
   )
 }
 
 const HeaderWrapper = styled.header`
-  padding: 30px;
+  padding: 0 5vw;
+`
+
+const Logo = styled.h1`
+  padding: 20px 0;
+`
+
+const SearchFormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 export default Header
